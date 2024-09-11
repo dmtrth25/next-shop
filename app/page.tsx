2e86f3f73d552d5cba7d17281b9b32ab440314f6
dmtrth25 <vodolazhchenko25@gmail.com>
2024-09-11 15:45:01 +0300

    created rangeSlider

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Title text="All products" size="lg" className="font-extrabold" />
       </Container>
       <HeaderBar />
-      <Container className="pb-14">
+      <Container className="mt-10 pb-14">
         <div className="flex gap-[60px]">
           {/**Filtration */}
           <div className="w-[250px]">
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,4 +1,5 @@
-import { FilterCheckbox, Title } from './'
+import { Input } from '../ui'
+import { FilterCheckbox, Title, RangeSlider } from './'
 
 interface IFiltersProps {
   className?: string
@@ -12,6 +13,15 @@ export const Filters: FC<IFiltersProps> = ({ className }) => {
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Can get" value="1" />
         <FilterCheckbox text="Newest" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Price (from - to):</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
+          <Input type="number" min={100} max={1000} placeholder="1000" />
+        </div>
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
       </div>
     </div>
   )
